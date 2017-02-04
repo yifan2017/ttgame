75e5e8f377e7942726220117ea421a6e0821c977
yifan2017 <mygirl99@126.com>
2017-02-04 16:29:32 +0800

    安装了drush en commerce_rules_extra 模块

---
+++ sites/all/modules/commerce_rules_extra/templates/form--cre_condition.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Template file for the condition form.
+ */
+
+$form['parameter']['term_id']['settings']['term_operator'] = $form['parameter']['term_operator']['settings']['term_operator'];
+$form['parameter']['term_id']['settings'] = array_reverse($form['parameter']['term_id']['settings'], TRUE);
+unset($form['parameter']['term_operator']);
+print drupal_render_children($form);
